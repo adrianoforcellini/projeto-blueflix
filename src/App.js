4207722efefb6a1676c40abbd0412046a58a524b
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-08 23:11:05 -0300

    estilizaçao degrade da imagem de capa, identificaçao e exibiçao das informaçoes de destaque

--- src/App.js
+++ src/App.js
@@ -9,16 +9,15 @@ export default () => {
   const [movieList, setMovieList] = useState([]);
   const [futuredData, setfuturedData] = useState(null);
 
-  const test = [1, 2, 3, 4];
-
   useEffect(() => {
     const loadAll = async () => {
       let list = await TMDB.getHomeList();
       setMovieList(list);
-       let originals = list.filter((item) => item.slug === 'originals')
-       let randomChosen = Math.floor(Math.random()*(originals[0].items.results.length));
-       let chosen = originals[0].items.results[randomChosen];
-       console.log(chosen)
+      let originals = list.filter((item) => item.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenData = await TMDB.getMovieInfo(chosen.id, 'tv');
+      setfuturedData(chosenData);
     }
     loadAll();
   }, []);
@@ -26,10 +25,10 @@ export default () => {
   return (
     <div className="page">
       <header><h1>BLUEFLIX</h1></header>
-     {futuredData &&  < FeaturedMovie item={futuredData}/>}
+      {futuredData && < FeaturedMovie item={futuredData} />}
       <section className="lists">
         {movieList.map((item, key) => (
-          <MovieRow key={key} title={item.title} items={item.items}/>))}
+          <MovieRow key={key} title={item.title} items={item.items} />))}
       </section>
     </div>
   );
